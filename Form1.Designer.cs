7f683fe1f047195dd8530ff7d4af62779344bf63
tl493 <tl493@LAB-M318-20>
2026-06-26 12:11:39 +1200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Learning_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addExpense = new System.Windows.Forms.Button();
+            this.expenseBox = new System.Windows.Forms.TextBox();
+            this.quantity = new System.Windows.Forms.NumericUpDown();
+            this.total = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.quantity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // addExpense
+            // 
+            this.addExpense.Location = new System.Drawing.Point(534, 103);
+            this.addExpense.Name = "addExpense";
+            this.addExpense.Size = new System.Drawing.Size(89, 29);
+            this.addExpense.TabIndex = 0;
+            this.addExpense.Text = "add";
+            this.addExpense.UseVisualStyleBackColor = true;
+            this.addExpense.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // expenseBox
+            // 
+            this.expenseBox.Location = new System.Drawing.Point(109, 102);
+            this.expenseBox.Name = "expenseBox";
+            this.expenseBox.Size = new System.Drawing.Size(151, 20);
+            this.expenseBox.TabIndex = 1;
+            this.expenseBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // quantity
+            // 
+            this.quantity.Location = new System.Drawing.Point(292, 103);
+            this.quantity.Name = "quantity";
+            this.quantity.Size = new System.Drawing.Size(53, 20);
+            this.quantity.TabIndex = 2;
+            // 
+            // total
+            // 
+            this.total.AutoSize = true;
+            this.total.Location = new System.Drawing.Point(172, 177);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(35, 13);
+            this.total.TabIndex = 3;
+            this.total.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(656, 284);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(292, 229);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(331, 150);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.quantity);
+            this.Controls.Add(this.expenseBox);
+            this.Controls.Add(this.addExpense);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.quantity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addExpense;
+        private System.Windows.Forms.TextBox expenseBox;
+        private System.Windows.Forms.NumericUpDown quantity;
+        private System.Windows.Forms.Label total;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Learning_GUI
+{
+    public partial class Form1 : Form
+    {
+        public List<Expense> expenses = new List<Expense>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(expenseBox.Text))
+            {
+                MessageBox.Show("Please enter your expense");
+                return;
+            }
+            if(quantity.Value == 0)
+            {
+                MessageBox.Show("Plese enter your quantity");
+                return;
+            }
+
+            expenses.Add(new Expense(expenseBox.Text, quantity.Value));
+
+
+            RefreshListBox(listBox1, expenses);
+            RefreshDgv(dataGridView1, expenses);
+        }
+
+        private void RefreshListBox<T>(ListBox listBox, List<T> list)
+        {
+            listBox.DataSource = null;
+            listBox.DataSource = list;
+        }
+
+        private void RefreshDgv<T>(DataGridView view, List<T> list)
+        {
+            view.DataSource = null;
+            view.DataSource = list;
+        }
+    }
+
+    public class Expense
+    {
+        public string Name { get; set; }
+        public decimal Quantity { get; set; }
+
+        public Expense(string name, decimal quantity)
+        {
+            Name = name;
+            Quantity = quantity;
+        }
+    }
+}
